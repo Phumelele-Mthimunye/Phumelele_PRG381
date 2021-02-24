877d1e4435dac91d5503b0fe8b31c5ba3ab39a1c
Phumelele-Mthimunye <mrgeminiaura@gmail.com>
2021-02-24 10:47:43 +0200

    New print line

--- PRG381.java
+++ PRG381.java
@@ -6,5 +6,6 @@ public class PRG381 {
     public static void main(String[] args) {
         System.out.println("First Git Repository");
         //comment
+        System.out.println("First Git Repository");
     }
 }
